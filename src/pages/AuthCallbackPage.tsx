e99dee041c74f44be1a70270586725ce09488c6c
BazaMedNafea <99833642+BazaMedNafea@users.noreply.github.com>
2024-06-04 20:24:32 +0100

    fixed returnTo page

--- src/pages/AuthCallbackPage.tsx
+++ src/pages/AuthCallbackPage.tsx
@@ -6,7 +6,7 @@ import WelcomePage from "./WelcomePage";
 
 const AuthCallbackPage = () => {
   const navigate = useNavigate();
-  const { user, isAuthenticated } = useAuth0(); // Get the authentication status from Auth0
+  const { user, isAuthenticated } = useAuth0();
   const { createUser } = useCreateMyUser();
   const { currentUser } = useGetMyUser();
   const hasCreatedCustomer = useRef(false);
@@ -14,7 +14,6 @@ const AuthCallbackPage = () => {
 
   useEffect(() => {
     const handleCallback = async () => {
-      // Check if the user is authenticated
       if (isAuthenticated) {
         if (user && !hasCreatedCustomer.current) {
           console.log("Creating new user...");
@@ -30,7 +29,7 @@ const AuthCallbackPage = () => {
           });
           hasCreatedCustomer.current = true;
         }
-        // Check if the current user has a type set to null
+
         if (currentUser) {
           console.log("Current user data:", currentUser);
           if (currentUser.customer || currentUser.provider === null) {
@@ -38,7 +37,7 @@ const AuthCallbackPage = () => {
             navigate("/choose-type");
           } else {
             console.log("User type is not null, navigating to previous page.");
-            navigate(returnTo);
+            navigate(returnTo); // Redirect to the returnTo path from local storage
           }
         } else {
           console.log("Current user data is not available.");
@@ -46,12 +45,12 @@ const AuthCallbackPage = () => {
       } else {
         console.log("User is not authenticated.");
         // Handle the case when the user is not authenticated
-        // You could navigate to the login page or show an error message
+        // Navigate to login page or show error message
       }
     };
 
     handleCallback();
-  }, [createUser, currentUser, navigate, user, isAuthenticated]);
+  }, [createUser, currentUser, navigate, user, isAuthenticated, returnTo]);
 
   return (
     <>
--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -2,7 +2,7 @@ const WelcomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#f7f7f7]">
       <h2 className="text-2xl font-bold mb-6 text-center text-[#222831]">
-        Welcome to Our Platform!
+        Welcome to Mhenni!
       </h2>
     </div>
   );
